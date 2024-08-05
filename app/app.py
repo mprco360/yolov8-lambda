8d3275f26cf78cd2a972ef86d028374ae1456caf
sonu <mrunaldharb23@vt.edu>
2024-08-05 16:17:49 -0400

    adding SQS

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ import PIL
 from PIL import Image
 from io import BytesIO
 import boto3
-import asyncio
 
 
 
@@ -49,13 +48,19 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
     s3_client = boto3.resource('s3')
+    print(event)
+    for record in event["Records"]:
+        tmp_rec = record
     try:
-        data = json.loads(event["body"])
+        data = json.loads(tmp_rec["body"])
     except:
-        data = event["body"]
+        if "body" in tmp_rec:
+            data = tmp_rec["body"]
+        else:
+            data = tmp_rec
+    print(data)
     if data:
-        if "image_url" in data and "source_dir" in data and "destination_dir" in data:
-            image_url = data["image_url"]
+        if "image" in data and "source_dir" in data and "destination_dir" in data:
             session_id = data["session_id"]
             source = data["source_dir"]
             destination = data["destination_dir"]
@@ -73,12 +78,8 @@ def lambda_handler(event, context):
                     "width":4,
                     "threshold":75
                 }
-            
-            try :
-                response = requests.get(image_url)
-            except:
-                raise InterruptedError("Image not found")
-            img = Image.open(BytesIO(response.content))
+            decoded_bytes = bytes.fromhex(data["image"])
+            img = Image.open(BytesIO(decoded_bytes))
             #img = img.resize((640,480))b6948ae9779f
             image_dict = {"session_id":session_id,"image":img}
             detected_objects,img = p.process(object_dict,imagemode=True,image_dict=image_dict)
